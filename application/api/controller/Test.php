03a943de3cf455c33a113eac8db2258e9417156b
zhangqq <c3663916@gmail.com>
2018-07-03 23:01:03 +0800

    tp5 frame

---
+++ application/api/controller/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiqizhang
+ * Date: 2018/7/3
+ * Time: 下午10:33
+ */
+
+namespace app\api\controller;
+
+
+class Test
+{
+    public function test(){
+        echo 'hello world!';
+    }
+}
