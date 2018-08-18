851317e594969f79bb89a095e0e66348fd0cbd4e
zhangqq <c3663916@gmail.com>
2018-08-18 10:46:40 +0800

    interface:theme

--- application/api/controller/v1/Theme.php
+++ application/api/controller/v1/Theme.php
@@ -11,6 +11,7 @@ namespace app\api\controller\v1;
 
 use app\api\validate\IDCollection;
 use app\api\model\Theme as ThemeModel;
+use app\api\validate\IDMustBePositiveInt;
 use app\lib\exception\ThemeException;
 
 class Theme
@@ -30,6 +31,16 @@ class Theme
             throw new ThemeException();
         }
         return $result;
+    }
+
+    public function getComplexOne($id)
+    {
+        (new IDMustBePositiveInt())->goCheck();
+        $theme = ThemeModel::getThemeWithProduct($id);
+        if(!$theme) {
+            throw new ThemeException();
+        }
+        return $theme;
     }
 
 }
